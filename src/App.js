2c79bb52f5d89905c41736b3c288012652c87aec
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-03-30 19:34:33 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Coins from "./components/Coins/Coins";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import Homepage from "./components/Homepage/Homepage";
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route path="/coins" element={<Coins></Coins>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
