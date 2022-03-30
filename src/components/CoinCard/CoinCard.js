1372561a3971ba652cf7622f0dadb2ecd74b912a
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-03-31 00:00:29 +0600

    coin card added

--- src/components/CoinCard/CoinCard.js
+++ src/components/CoinCard/CoinCard.js
@@ -7,10 +7,15 @@ const CoinCard = ({ coin }) => {
       <Link to="/coins">
         <div className="flex gap-4 justify-between items-center">
           <div className="flex-shrink-0">
-            <img className="mx-auto object-cover rounded-full h-16 w-16" src={coin.image} alt="coinImg" />
+            <img
+              className="mx-auto object-cover rounded-full h-16 w-16"
+              src={coin.image}
+              alt="coinImg"
+            />
           </div>
           <div className="flex flex-col justify-end">
-            <small className="text-gray-600 font-medium">{coin.name}</small>
+            <span className="text-gray-600 font-medium">{coin.name}</span>
+            <span className="text-gray-400 text-xs">{coin.symbol}</span>
           </div>
         </div>
       </Link>
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -20,7 +20,7 @@ const Coins = () => {
         Total coins:{coins.length}
       </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-7">
         {coins.map((coin) => (
           <CoinCard key={coin.id} coin={coin}></CoinCard>
         ))}
