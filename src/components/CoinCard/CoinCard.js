8afeccd546caea8e98e9cbf3082f2d48ddb83a38
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-03-30 21:10:37 +0600

    cripto card added

--- src/components/CoinCard/CoinCard.js
+++ src/components/CoinCard/CoinCard.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CoinCard = ({ coin }) => {
   return (
-    <div>
-      <p>{coin.id}</p>
-      <p>{coin.name}</p>
+    <div className="shadow-lg rounded-2xl w-[250px] bg-white p-4">
+      <Link to="/coins">
+        <div className="flex gap-4 justify-between items-center">
+          <div className="flex-shrink-0">
+            <img className="mx-auto object-cover rounded-full h-16 w-16" src={coin.image} alt="coinImg" />
+          </div>
+          <div className="flex flex-col justify-end">
+            <small className="text-gray-600 font-medium">{coin.name}</small>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -11,13 +11,20 @@ const Coins = () => {
       .then((data) => setCoins(data));
   }, []);
   return (
-
     <div className="px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2 ">
-        <p className="text-center text-3xl font-bold text-gray"> Available Crypto Curencies</p>
-        <p className="text-center mb-12 m-4 text-xl font-normal text-gray-500">Total coins:{coins.length}</p>
-      {coins.map((coin) => (
-        <CoinCard key={coin.id} coin={coin}></CoinCard>
-      ))}
+      <p className="text-center text-3xl font-bold text-gray">
+        {" "}
+        Available Crypto Curencies
+      </p>
+      <p className="text-center mb-12 m-4 text-xl font-normal text-gray-500">
+        Total coins:{coins.length}
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
+        {coins.map((coin) => (
+          <CoinCard key={coin.id} coin={coin}></CoinCard>
+        ))}
+      </div>
     </div>
   );
 };
